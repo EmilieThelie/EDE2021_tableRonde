d9246a901047aa73d61f60eb4ac64620730e4ca9
rwicker <raphael.wicker@ias.u-psud.fr>
2021-08-19 17:49:10 +0200

    First bits of code, reads the data

--- Creux_WL.py
+++ Creux_WL.py
@@ -1,6 +1,22 @@
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from astropy.cosmology import Planck18
 
 
-print('bla')
-print('blabla')
-print('blablabla')
+data = pd.read_csv('/home/raphael/Téléchargements/result_cu1jemw77zite8se.csv')
+cleaned_data = data[(data['MAG_r'] != 99.) & (data['MAG_i'] != 99.)]
+
+print('Do you want to plot the data ? (y/n)')
+plut = input()
+if plot == 'y' :
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.hist(cleaned_data['Z_B'], bins = 100)
+print(cleaned_data)
+
+def slice_data(lower, upper):
+    return binned_data
+
+def main():
+    return 0

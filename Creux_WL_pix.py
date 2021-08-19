885b481c18157e94124e2e16e68bfedf783ae3c7
Emilie Thelie <ethelie@MacBook-Pro-de-obs.local>
2021-08-19 19:20:26 +0200

    density map in fits

--- Creux_WL_pix.py
+++ Creux_WL_pix.py
@@ -2,18 +2,16 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from astropy.cosmology import Planck18
+import astropy.io.fits as fits
 
 ### data
-data = pd.read_csv('/home/raphael/Téléchargements/result_cu1jemw77zite8se.csv')
+data = pd.read_csv('../../shared_folder/result_cu1jemw77zite8se.csv')
 cleaned_data = data[(data['MAG_r'] != 99.) & (data['MAG_i'] != 99.)]
 RAmin, RAmax = 207,222
 DECmin, DECmax = 50, 58
 
-### density map
-Npix = 100
-
-print('Do you want to plot the data ? (y/n)')
-plot = input()
+#print('Do you want to plot the data ? (y/n)')
+plot = 'n'#input()
 if plot == 'y' :
     fig = plt.figure()
     ax = fig.add_subplot(111)
@@ -30,11 +28,21 @@ def slice_data(lower, upper, plot = False): #bins : 0.3-0.5, 0.7-0.9
         ax.set_ylabel(r'$\delta_{J2000}$(°)')
         ax.set_title(f'Galaxy distribution between z = {lower} and z = {upper}')
     return binned_data
+    
+### density map
+RA_Nbins, DEC_Nbins = 80, 150
+lower_z, higher_z = 0.3, 0.5
+data_in_z_bin = cleaned_data[(cleaned_data['Z_B'] > lower_z) & (cleaned_data['Z_B'] < higher_z)]
+data_map, edges = np.histogramdd((data_in_z_bin['ALPHA_J2000'],data_in_z_bin['DELTA_J2000']),bins=(RA_Nbins,DEC_Nbins))
+fits.writeto('density_map.fits',data_map)
+def plot_density_map():
+    plt.figure()
+    plt.imshow(data_map.T,origin='lower',cmap='jet',extent=(RAmin,RAmax,DECmin,DECmax),aspect='auto')
+    plt.xlabel(r'$\alpha_{J2000}$')
+    plt.ylabel(r'$\delta_{J2000}$')
+    plt.colorbar(label='$N_{gal}$')
+    plt.show()
+
 
 if __name__=="__main__":
-    lower_z, higher_z = 0.3, 0.5
-    data_in_z_bin = cleaned_data[(cleaned_data['Z_B'] > lower_z) & (cleaned_data['Z_B'] < higher_z)].value
-    
-    data_map = np.zeros((Npix,Npix))
-    
-    data_map = np.histogram(data_in_z_bin,bins=np.linspace(data_in_z_bin.min(),data_in_z_bin.max(),))
+    print()

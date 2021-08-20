27405fd0c0455e117f96e1bd751b66b0fb3d9b19
Emilie Thelie <ethelie@MacBook-Pro-de-obs.local>
2021-08-20 10:23:04 +0200

    encore avec les vides

--- Creux_WL_pix.py
+++ Creux_WL_pix.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from astropy.cosmology import Planck18
 import astropy.io.fits as fits
+from scipy.ndimage import gaussian_filter
 
 ### data
 data = pd.read_csv('../../shared_folder/result_cu1jemw77zite8se.csv')
@@ -47,8 +48,37 @@ def plot_density_map():
 w = np.where(data_map==0)
 mask = np.zeros((RA_Nbins, DEC_Nbins))
 mask[w] = 1
-fits.writeto('mask.fits',mask)
+#fits.writeto('mask.fits',mask)
+def plot_mask():
+    plt.figure()
+    plt.imshow(mask.T,origin='lower',cmap='binary',extent=(RAmin,RAmax,DECmin,DECmax),aspect='auto')
+    plt.xlabel(r'$\alpha_{J2000}$')
+    plt.ylabel(r'$\delta_{J2000}$')
+    plt.colorbar(label='$N_{gal}$')
+    plt.show()
+
+
+### critical points
+pers = 1
+f = np.load("density_map_adims_sourceMinDownSkl_"+str(pers)+".npz")
+crit_pos = f["crit_pos"]
+crit_type = f["crit_type"]
+def plot_density_map_cp():
+    plt.figure()
+    data_map_smoothed = smoothed_field = gaussian_filter(data_map,sigma=1)
+    plt.imshow(data_map_smoothed.T,origin='lower',cmap='jet',aspect='auto')#,extent=(RAmin,RAmax,DECmin,DECmax),aspect='auto')
+    plt.xlabel(r'$\alpha_{J2000}$')
+    plt.ylabel(r'$\delta_{J2000}$')
+    plt.colorbar(label='$N_{gal}$')
+    
+    w = np.where(crit_type==0)
+    plt.scatter(crit_pos[1,w],crit_pos[0,w],marker="*",color="k")
+    
+    plt.title("pers="+str(pers))
+    plt.show()
 
 
 if __name__=="__main__":
-    print()
+    plot_density_map_cp()
+    #plot_mask()
+    

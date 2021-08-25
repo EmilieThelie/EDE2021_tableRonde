e682114e852d130406bc747bef25a14d02a3e199
Emilie Thelie <ethelie@MacBook-Pro-de-obs.local>
2021-08-25 09:55:13 +0200

    vrais vides

--- Creux_WL_pix.py
+++ Creux_WL_pix.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from astropy.cosmology import Planck18
 import astropy.io.fits as fits
 from scipy.ndimage import gaussian_filter
+import matplotlib
 
 ### data
 data = pd.read_csv('../../shared_folder/result_cu1jemw77zite8se.csv')
@@ -36,7 +37,7 @@ lower_z, higher_z = 0.3, 0.5
 data_in_z_bin = cleaned_data[(cleaned_data['Z_B'] > lower_z) & (cleaned_data['Z_B'] < higher_z)]
 data_map, edges = np.histogramdd((data_in_z_bin['ALPHA_J2000'],data_in_z_bin['DELTA_J2000']),bins=(RA_Nbins,DEC_Nbins))
 #fits.writeto('density_map.fits',data_map)
-def plot_density_map():
+def plot_density_map(data_map):
     plt.figure()
     plt.imshow(data_map.T,origin='lower',cmap='jet',extent=(RAmin,RAmax,DECmin,DECmax),aspect='auto')
     plt.xlabel(r'$\alpha_{J2000}$')
@@ -51,24 +52,40 @@ mask = np.zeros((RA_Nbins, DEC_Nbins))
 mask[w] = 1
 #fits.writeto('mask.fits',mask.flatten())
 # dtfe map
-data_map = fits.getdata('dtfe_map_z_0.3_0.5_512x512.fits')
-w = np.where(data_map==0)
+zmin, zmax = 0.7, 0.9
+data_map = fits.getdata('dtfe_map_z_'+str(zmin)+'_'+str(zmax)+'_512x512.fits')
+percentile = 15
+w = np.where(data_map<=np.percentile(data_map,percentile))
 mask = np.zeros((data_map.shape[0], data_map.shape[1]))
 mask[w] = 1
-flat_mask = mask.flatten()
+#flat_mask = mask.flatten()
 #fits.writeto('dtfe_map_z_0.3_0.5_512x512_mask.fits',flat_mask)
+#matplotlib.image.imsave('dtfe_map_z_0.3_0.5_512x512_mask.png',mask)
+
+### mean density in mask
+data_map2 = np.copy(data_map)
+data_map2[w] = data_map.mean()
+#fits.writeto('dtfe_map_z_0.3_0.5_512x512_meanDinMask.fits',data_map2)
+
+### max density in mask
+data_map2 = np.copy(data_map)
+data_map2[w] = data_map.max()
+#fits.writeto('dtfe_map_z_'+str(zmin)+'_'+str(zmax)+'_512x512_maxDinMask.fits',data_map2)
 
 def plot_mask(mask):
     plt.figure()
-    plt.imshow(mask.T,origin='lower',cmap='binary',extent=(RAmin,RAmax,DECmin,DECmax),aspect='auto')
+    #plt.imshow(data_map,origin='lower',cmap='jet',extent=(RAmin,RAmax,DECmin,DECmax),aspect='auto')
+    #plt.imshow(mask,origin='lower',cmap='binary_r',extent=(RAmin,RAmax,DECmin,DECmax),aspect='auto',alpha=0.4)
+    plt.imshow(mask,origin='lower',cmap='binary',extent=(RAmin,RAmax,DECmin,DECmax),aspect='auto')
     plt.xlabel(r'$\alpha_{J2000}$')
     plt.ylabel(r'$\delta_{J2000}$')
     plt.colorbar(label='$N_{gal}$')
     plt.show()
+    
 
 ### critical points
-pers = 1
-f = np.load('dtfe_map_z_0.3_0.5_512x512_adims3_sourceMinDownSklMasked_'+str(pers)+'.npz') #'density_map_adims_sourceMinDownSkl_'+str(pers)+'.npz'
+pers = 0.15
+f = np.load('dtfe_map_z_'+str(zmin)+'_'+str(zmax)+'_512x512_maxDinMask_adims3_sourceMinDownSkl_'+str(pers)+'.npz')
 crit_pos = f['crit_pos']
 crit_type = f['crit_type']
 def plot_density_map_cp():
@@ -79,19 +96,27 @@ def plot_density_map_cp():
     plt.ylabel(r'$\delta_{J2000}$')
     plt.colorbar(label=r'$\rho$')
     
-    w = np.where(crit_type==0)
-    X = RAmin + crit_pos[1,w] * (RAmax-RAmin)/data_map_smoothed.shape[0]
-    Y = DECmin + crit_pos[0,w] * (DECmax-DECmin)/data_map_smoothed.shape[1]
-    plt.scatter(X,Y,marker='*',color='k')
+    w = np.where(crit_type==0)[0]
+    #X = RAmin + crit_pos[1,w] * (RAmax-RAmin)/data_map_smoothed.shape[0]
+    #Y = DECmin + crit_pos[0,w] * (DECmax-DECmin)/data_map_smoothed.shape[1]
+    X = RAmin + crit_pos[0,w] * (RAmax-RAmin)/data_map.shape[0]
+    Y = DECmin + crit_pos[1,w] * (DECmax-DECmin)/data_map.shape[1]
+    plt.scatter(X,Y,marker='*',s=150,color='magenta')
     #plt.scatter(crit_pos[1,w],crit_pos[0,w],marker="*",color="k")
     
     plt.title('pers='+str(pers))
     plt.show()
 
+# saving the min in RA,DEC coordinates
+w = np.where(crit_type==0)[0]
+X = RAmin + crit_pos[0,w] * (RAmax-RAmin)/data_map.shape[0]
+Y = DECmin + crit_pos[1,w] * (DECmax-DECmin)/data_map.shape[1]
+np.savez('dtfe_map_z_'+str(zmin)+'_'+str(zmax)+'_512x512_maxDinMask_voids.npz',RA=X,DEC=Y)
 
 if __name__=='__main__':
-    plot_density_map_cp()
     #plot_mask(mask)
+    #plot_density_map(data_map2)
+    plot_density_map_cp()
     
     
     
